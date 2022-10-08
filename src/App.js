90a16e35242175c3898ba9c0dfc633071402cb2b
ipeerless <ipeerless0@gmail.com>
2022-10-08 22:48:03 +0200

    go

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from "react";
+import { useEffect } from "react";
+import './App.css'
+import searchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+// 6940f497
+const API_URL = 'http://www.omdbapi.com?apikey=6940f497'
+const movie1 = {
+    'Title': 'Amazing Spiderman Syndrome',
+    'Year': '2012',
+    'imdbID': 'tt2586634',
+    'Type': 'movie',
+    'Poster': 'N/A'
 }
+const App = ()  => {
+    const [movies, setMovies] = useState([])
+    const search = async (title) => {
+        const response = await fetch('${API_URL}&s=${title}')
+        const data = await response.json()
+        setMovies.data.search()
 
-export default App;
+    }
+    useEffect(()=> {
+
+    },[])
+    return (
+       <div className="app">
+        <h1>MovieLand</h1>
+        <div className="search">
+            <input
+            placeholder="Search for movie"
+            value='Superman'
+            onChange={()=>{}}
+            />
+            <img 
+            src={searchIcon}
+            alt= 'Search'
+            onClick={()=>{}}
+            />
+
+
+        </div>
+        {
+            movies?.length > 0
+            ? (
+                <div className="container">
+          {movies.map((movie)=> {
+            <MovieCard movie={movie}/>
+
+          })}
+        </div>
+        ) : (
+            <div className="empty">
+                <h2>No movies found</h2>
+
+            </div>
+        )
+
+
+            
+            
+        
+        }
+        
+       </div>
+    )
+}
+export default App
